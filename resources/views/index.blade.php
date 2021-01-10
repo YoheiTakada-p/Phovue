a2ea9929f47658d61ac9a1bed716eb44f5d35626
yohei <cha.chu.you.program@gmail.com>
2021-01-10 15:28:04 +0900

    add vue-router vuex

---
+++ resources/views/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
+    <!-- style -->
+    <style>
+        .btn-gray {
+            color: #fff;
+            background-color: #1e202259;
+        }
+
+        .photo-form {
+            min-width: 25rem;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div id="app">
+        <Sample></Sample>
+    </div>
+</body>
+
+</html>
