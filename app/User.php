78918e3bbb620faea6fa0fbbf83bb88af9ff3ff7
yohei <cha.chu.you.program@gmail.com>
2021-01-13 17:15:04 +0900

    add model User

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * リレーション - photos
+     */
+    public function photos()
+    {
+        return $this->hasMany('App\Photo');
+    }
 }
