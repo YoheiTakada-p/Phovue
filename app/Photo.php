fd7d9dd47674e31ffb54d3f4a8bffc5b3978f458
yohei <cha.chu.you.program@gmail.com>
2021-01-13 17:13:39 +0900

    create model Photo

---
+++ app/Photo.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    //プライマリキーの型
+    protected $keyType = 'string';
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        if (!Arr::get($this->attributes, 'id')) {
+            $this->setId();
+        }
+    }
+
+    /**
+     * ランダムIDを生成して代入する
+     */
+    public function setId()
+    {
+        $this->attributes['id'] = $this->getRandomId();
+    }
+
+    /**
+     * ランダムIDを生成する関数
+     */
+    public function getRandomId()
+    {
+        return \Str::random(12);
+    }
+}
