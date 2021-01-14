ebd9770476d357a6973db39a84af0ca3b545e950
yohei <cha.chu.you.program@gmail.com>
2021-01-14 14:51:22 +0900

    create vue Photocard

--- public/js/app.js
+++ public/js/app.js
@@ -2558,6 +2558,38 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/pages/PhotoList.vue?vue&type=script&lang=js&":
+/*!***************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/pages/PhotoList.vue?vue&type=script&lang=js& ***!
+  \***************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_PhotoCard__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../components/PhotoCard */ "./resources/js/components/PhotoCard.vue");
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  components: {
+    PhotoCard: _components_PhotoCard__WEBPACK_IMPORTED_MODULE_0__["default"]
+  },
+  data: function data() {
+    return {
+      item: null
+    };
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/bootstrap/dist/js/bootstrap.js":
 /*!*****************************************************!*\
   !*** ./node_modules/bootstrap/dist/js/bootstrap.js ***!
@@ -39302,6 +39334,95 @@ render._withStripped = true
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/PhotoCard.vue?vue&type=template&id=34b26d87&":
+/*!************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/PhotoCard.vue?vue&type=template&id=34b26d87& ***!
+  \************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _vm._m(0)
+}
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "card" }, [
+      _c("div", { staticClass: "position-relative" }, [
+        _c("img", { staticClass: "card-img", attrs: { alt: "..." } }),
+        _vm._v(" "),
+        _c("a", {
+          staticClass: "card-img-overlay",
+          attrs: {
+            "data-toggle": "collapse",
+            href: "#collapse-comment-0",
+            role: "button",
+            "aria-expanded": "false",
+            "aria-controls": "collapse-comment-0"
+          }
+        })
+      ]),
+      _vm._v(" "),
+      _c(
+        "div",
+        { staticClass: "card-body", staticStyle: { padding: "0.7rem" } },
+        [
+          _c(
+            "div",
+            { staticClass: "collapse", attrs: { id: "collapse-comment-0" } },
+            [
+              _c("div", [
+                _c("p", { staticClass: "text-justify" }, [
+                  _vm._v("サンプルコメント")
+                ])
+              ])
+            ]
+          ),
+          _vm._v(" "),
+          _c("div", { staticClass: "d-flex justify-content-between" }, [
+            _c("span", { staticClass: "my-auto" }, [_vm._v("name")]),
+            _vm._v(" "),
+            _c("div", [
+              _c("button", { staticClass: "btn btn-sm btn-gray" }, [
+                _c("i", {
+                  staticClass: "fas fa-heart",
+                  staticStyle: { color: "pink" }
+                }),
+                _vm._v("1\n        ")
+              ]),
+              _vm._v(" "),
+              _c(
+                "a",
+                { staticClass: "btn btn-sm btn-gray", attrs: { href: "#" } },
+                [
+                  _c("i", {
+                    staticClass: "fas fa-download",
+                    staticStyle: { color: "white" }
+                  })
+                ]
+              )
+            ])
+          ])
+        ]
+      )
+    ])
+  }
+]
+render._withStripped = true
+
+
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/PhotoForm.vue?vue&type=template&id=516c053b&":
 /*!************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/PhotoForm.vue?vue&type=template&id=516c053b& ***!
@@ -39675,89 +39796,11 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c("div", { staticClass: "container mt-3" }, [
+    _c("div", { staticClass: "card-columns" }, [_c("PhotoCard")], 1)
+  ])
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "container mt-3" }, [
-      _c("div", { staticClass: "card-columns" }, [
-        _c("div", { staticClass: "card" }, [
-          _c("div", { staticClass: "position-relative" }, [
-            _c("img", {
-              staticClass: "card-img",
-              attrs: { src: "", alt: "..." }
-            }),
-            _vm._v(" "),
-            _c("a", {
-              staticClass: "card-img-overlay",
-              attrs: {
-                "data-toggle": "collapse",
-                href: "#collapse-comment-0",
-                role: "button",
-                "aria-expanded": "false",
-                "aria-controls": "collapse-comment-0"
-              }
-            })
-          ]),
-          _vm._v(" "),
-          _c(
-            "div",
-            { staticClass: "card-body", staticStyle: { padding: "0.7rem" } },
-            [
-              _c(
-                "div",
-                {
-                  staticClass: "collapse",
-                  attrs: { id: "collapse-comment-0" }
-                },
-                [
-                  _c("div", [
-                    _c("p", { staticClass: "text-justify" }, [
-                      _vm._v(
-                        "\n              Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium\n              purus sit amet fermentum. Donec sed odio operae, eu vulputate\n              felis rhoncus. Praeterea iter est quasdam res quas ex communi.\n              At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi\n              concilium totius Galliae in diem certam indicere. Cras mattis\n              iudicium purus sit amet fermentum.\n            "
-                      )
-                    ])
-                  ])
-                ]
-              ),
-              _vm._v(" "),
-              _c("div", { staticClass: "d-flex justify-content-between" }, [
-                _c("span", { staticClass: "my-auto" }, [_vm._v("name")]),
-                _vm._v(" "),
-                _c("div", [
-                  _c("button", { staticClass: "btn btn-sm btn-gray" }, [
-                    _c("i", {
-                      staticClass: "fas fa-heart",
-                      staticStyle: { color: "pink" }
-                    }),
-                    _vm._v("1\n            ")
-                  ]),
-                  _vm._v(" "),
-                  _c(
-                    "a",
-                    {
-                      staticClass: "btn btn-sm btn-gray",
-                      attrs: { href: "#" }
-                    },
-                    [
-                      _c("i", {
-                        staticClass: "fas fa-download",
-                        staticStyle: { color: "white" }
-                      })
-                    ]
-                  )
-                ])
-              ])
-            ]
-          )
-        ])
-      ])
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -56822,6 +56865,59 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/js/components/PhotoCard.vue":
+/*!***********************************************!*\
+  !*** ./resources/js/components/PhotoCard.vue ***!
+  \***********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _PhotoCard_vue_vue_type_template_id_34b26d87___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./PhotoCard.vue?vue&type=template&id=34b26d87& */ "./resources/js/components/PhotoCard.vue?vue&type=template&id=34b26d87&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
+  script,
+  _PhotoCard_vue_vue_type_template_id_34b26d87___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _PhotoCard_vue_vue_type_template_id_34b26d87___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/PhotoCard.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/PhotoCard.vue?vue&type=template&id=34b26d87&":
+/*!******************************************************************************!*\
+  !*** ./resources/js/components/PhotoCard.vue?vue&type=template&id=34b26d87& ***!
+  \******************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_PhotoCard_vue_vue_type_template_id_34b26d87___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib??vue-loader-options!./PhotoCard.vue?vue&type=template&id=34b26d87& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/PhotoCard.vue?vue&type=template&id=34b26d87&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_PhotoCard_vue_vue_type_template_id_34b26d87___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_PhotoCard_vue_vue_type_template_id_34b26d87___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
+
+/***/ }),
+
 /***/ "./resources/js/components/PhotoForm.vue":
 /*!***********************************************!*\
   !*** ./resources/js/components/PhotoForm.vue ***!
@@ -56970,15 +57066,17 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _PhotoList_vue_vue_type_template_id_401edab7___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./PhotoList.vue?vue&type=template&id=401edab7& */ "./resources/js/pages/PhotoList.vue?vue&type=template&id=401edab7&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _PhotoList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./PhotoList.vue?vue&type=script&lang=js& */ "./resources/js/pages/PhotoList.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _PhotoList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _PhotoList_vue_vue_type_template_id_401edab7___WEBPACK_IMPORTED_MODULE_0__["render"],
   _PhotoList_vue_vue_type_template_id_401edab7___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -56992,6 +57090,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "resources/js/pages/PhotoList.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/pages/PhotoList.vue?vue&type=script&lang=js&":
+/*!*******************************************************************!*\
+  !*** ./resources/js/pages/PhotoList.vue?vue&type=script&lang=js& ***!
+  \*******************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_PhotoList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./PhotoList.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/pages/PhotoList.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_PhotoList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
---
+++ resources/js/components/PhotoCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <div class="position-relative">
+      <img class="card-img" alt="..." />
+      <a
+        class="card-img-overlay"
+        data-toggle="collapse"
+        href="#collapse-comment-0"
+        role="button"
+        aria-expanded="false"
+        aria-controls="collapse-comment-0"
+      ></a>
+    </div>
+    <div class="card-body" style="padding: 0.7rem">
+      <div class="collapse" id="collapse-comment-0">
+        <div>
+          <p class="text-justify">サンプルコメント</p>
+        </div>
+      </div>
+      <div class="d-flex justify-content-between">
+        <span class="my-auto">name</span>
+        <div>
+          <button class="btn btn-sm btn-gray">
+            <i class="fas fa-heart" style="color: pink"></i>1
+          </button>
+          <a href="#" class="btn btn-sm btn-gray">
+            <i class="fas fa-download" style="color: white"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
